49bd10e34d57d236f6f68372c0bdd4d84ae6adb6
Ijalana Afolabi <iijalana@gmail.com>
2023-05-18 05:56:30 -0700

    update

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -2,5 +2,5 @@
 def square_matrix_simple(matrix=[]):
     newMtrix = []
     for x in matrix:
-        newMtrix.append(list(map(lambda y: y ** 2, x))) 
+        newMtrix.append(list(map(lambda y : y ** 2, x))) 
     return newMtrix
