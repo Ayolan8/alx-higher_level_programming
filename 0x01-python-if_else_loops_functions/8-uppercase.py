649b78e32d13bf75eebd031059c25266c679abba
Ijalana Afolabi <iijalana@gmail.com>
2023-11-01 10:42:09 -0700

    update

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def uppercase(str):
     for ch in str:
-        if ord(ch) > 96 and ord(ch) <=122:
+        if ord(ch) > 96 and ord(ch) <= 122:
             ch = chr(ord(ch) - 32)
         print("{:s}".format(ch), end='')
     print("")
