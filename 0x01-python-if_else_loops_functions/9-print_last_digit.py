bd33f79fce89940a4af921878f14b907a73e2481
Ijalana Afolabi <iijalana@gmail.com>
2023-11-01 10:50:01 -0700

    update

--- 0x01-python-if_else_loops_functions/9-print_last_digit.py
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python3
 def print_last_digit(number):
-    print("{}".format(abs(number) % 10), end=",")
-    return abs(number) % 10
+    if number < 0:
+        return number % -10
+    else:
+        return number % 10
