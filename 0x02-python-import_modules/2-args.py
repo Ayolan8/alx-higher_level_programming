357cdedfe84dff2334a4466ba9e5ed8e6688ecb2
Ijalana Afolabi <iijalana@gmail.com>
2023-11-07 03:40:51 -0800

    update

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     from sys import argv
-    argCount = len(argv)
-    if argCount <= 0:
-        print("0 arguments.")
+    argCount = len(argv) - 1
+    if argCount == 0:
+        print("{:d} arguments".format(argCount))
     else:
-        if argCount == 2:
-            print(argCount - 1, "argument:")
-        else:
-            print(argCount - 1, "arguments:")
-        for num, value in enumerate(argv[1:], start=1):
-            print("{} {}".format(num, value))
+        if argCount == 1:
+            print("{:d} argument:".format(argCount))
+        elif argCount > 1:
+            print("{} arguments:".format(argCount))
+        for num, item in enumerate(argv[1:], start=1):
+            print("{} {}".format(num, item))
