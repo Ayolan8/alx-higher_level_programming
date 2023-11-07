de34fa9a4243435e2438a4e6e3947c1a86f861a0
Ijalana Afolabi <iijalana@gmail.com>
2023-11-07 03:45:53 -0800

    update

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -8,6 +8,6 @@ if __name__ == "__main__":
         if argCount == 1:
             print("{:d} argument:".format(argCount))
         elif argCount > 1:
-            print("{} arguments:".format(argCount))
+            print("{:d} arguments:".format(argCount))
         for num, item in enumerate(argv[1:], start=1):
-            print("{} {}".format(num, item))
+            print("{:d} {:s}".format(num, item))
