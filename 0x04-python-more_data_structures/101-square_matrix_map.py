9e28dad5319ee7b35affaba3a48e325cc9725006
Ijalana AFOLABI <iijalana@gmail.com>
2023-11-10 09:30:33 -0800

    update

--- 0x04-python-more_data_structures/101-square_matrix_map.py
+++ 0x04-python-more_data_structures/101-square_matrix_map.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 def square_matrix_map(matrix=[]):
-    res = [list(map(lambda x: list(map(lambda y: y ** 2, x[:])), matrix))]
+    res = list(map(lambda x: list(map(lambda y: y ** 2, x[:])), matrix))
     return res
