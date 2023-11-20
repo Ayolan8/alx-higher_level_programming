085b65727d37609c05a80c6716ba7563271971e5
Ijalana AFOLABI <iijalana@gmail.com>
2023-11-20 12:06:21 -0800

    0-safe_print_list.py

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
+    returnElement = 0;
     try:
         for y in range(x):
             print(my_list[y], end="")
+            returnElement = returnElement + 1
     except IndexError:
         pass
     print()
-    return y
+    return returnElement
